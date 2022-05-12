aab7ffbdb4cdef7bcfd5c9914d1003dae1fb091e
MatheusTostes <matheustostes@hotmail.com>
2022-05-11 21:17:27 -0300

    Add create products function

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const url = 'https://catalognodejs2022-matheustostes.vercel.app/products/'
 let response = {}
 const tableElements = document.getElementById('products-table')
 const searchInput = document.getElementById('search-input')
-
+const btnCreateProduct = document.getElementById('btn-product-create')
+const btnClearInputs = document.getElementById('empty-create')
 
 const getApi = async () => {
     const responseFull = await axios.get(url)
@@ -17,13 +18,30 @@ const populateItems = async (data) => {
                     <p class='product-id'>${item.product_id}</p>
                     <button class='row-delete' onCLick='deleteProduct(event)'>X</button>
                 </div>
-                <p>nome: <input class='product-name' value=${item.name}></p>
-                <p>preço: <input class='product-price' value=${item.price}></p>
-                <p>descrição: <input class='product-description' value='${item.description}'></p>
-                <p>url da imagem: <input class='product-image' value=${item.image}></p>
-                <p>categoria: <input class='product-category' value=${item.category}></p>
-                <p>promoção: <input class='product-promo' value=${item.promo}></p>
-                <button class='row-update' onClick='productUpdateForm(event)'>ALTERAR</button>
+
+                
+
+                <form>
+                    <p>nome: <input class='product-name' value=${item.name} required></p>
+                    <p>preço: <input type="number" class='product-price' value=${item.price} required></p>
+                    <p>descrição: <input class='product-description' value='${item.description}' required></p>
+                    <p>url da imagem: <input class='product-image' value=${item.image} required></p>
+                    <p>categoria: 
+                        <select class='product-category' value=${item.category} required>
+                            <option value="lanches">Lanches</option>
+                            <option value="porções">Porções</option>
+                            <option value="bebidas">Bebidas</option>
+                            <option value="sobremesas">Sobremesas</option>
+                        </select>
+                    </p>
+                    <p>promoção: 
+                        <select class='product-promo' value=${item.promo} required>
+                            <option value="y">sim</option>
+                            <option value="n">não</option>
+                        </select>
+                    </p>
+                    <input type="submit" class='row-update' onClick='productUpdateForm(event)'>
+                 </form>
             </div>
         `
         tableElements.insertAdjacentHTML('beforeend', product)
@@ -43,7 +61,8 @@ const searchItems = () => {
 searchInput.addEventListener('input', searchItems)
 
 const productUpdateForm = (event) => {
-    const productElement = event.target.parentElement
+    event.preventDefault()
+    const productElement = event.target.parentElement.parentElement
     const product_id = parseInt(productElement.getElementsByClassName("product-id")[0].innerText)
     const name = productElement.getElementsByClassName("product-name")[0].value
     const price = productElement.getElementsByClassName("product-price")[0].value
@@ -61,9 +80,6 @@ const updateProduct = (product_id, data) => {
     const urlProduct =  url+product_id
     
     axios.put(urlProduct, data)
-    // .then(response => {
-    //     console.log('update');
-    // })
 }
 
 const deleteProduct = (event) => {
@@ -75,6 +91,40 @@ const deleteProduct = (event) => {
     axios.delete(urlProduct)
     product.remove()
 }
+
+const createProduct = async (event) => {
+    event.preventDefault()
+    const productElement = event.target.parentElement
+    const name = productElement.getElementsByClassName("product-name")[0].value
+    const price = productElement.getElementsByClassName("product-price")[0].value
+    const description = productElement.getElementsByClassName("product-description")[0].value
+    const image = productElement.getElementsByClassName("product-image")[0].value
+    const category = productElement.getElementsByClassName("product-category")[0].value
+    const promo = productElement.getElementsByClassName("product-promo")[0].value
+
+    const data = { name, price, description, image, category, promo }
+
+    if (name !== '' & price !== '' & description !== '' & image !== '') {
+        await axios.post(url, data)
+    
+        tableElements.innerHTML = ''
+        await getApi()
+        await populateItems(response)
+    }
+}
+btnCreateProduct.addEventListener('click', createProduct)
+
+const clearInputs = (event) => {
+    const productElement = event.target.parentElement.parentElement
+
+    // console.log(productElement);
+    productElement.getElementsByClassName("product-name")[0].value = ''
+    productElement.getElementsByClassName("product-price")[0].value = 0
+    productElement.getElementsByClassName("product-description")[0].value = ''
+    productElement.getElementsByClassName("product-image")[0].value = ''
+}
+btnClearInputs.addEventListener('click', clearInputs)
+
 
 window.onload = async () => {
     await getApi()
